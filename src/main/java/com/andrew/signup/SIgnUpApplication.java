d4bd5cdd4eb8c05f3af0246fdf3d03613e5cc8c4
Andrew Acheampong <gorgi.andy@gmail.com>
2025-03-20 00:03:28 -0400

    Initial commit

---
+++ src/main/java/com/andrew/signup/SIgnUpApplication.java
@@ -0,0 +1,13 @@
+package com.andrew.signup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SIgnUpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SIgnUpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/andrew/signup/SIgnUpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.andrew.signup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SIgnUpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
